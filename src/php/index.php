1beb1671153fcd91b77b4956be8b28add4e496a8
Panda Js <alimuradov1991hunt@mail.ru>
2020-06-14 23:44:59 +0300

    first commit in this branch

---
+++ src/php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PandaJS</title>
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+
+<body>
+<h1>It's just testing page.</h1>
+
+
+    <script src="../js/demo.js"></script> 
+</body>
+
+</html>
